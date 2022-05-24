f136c82c42ead708582f9d04b26e8cb475c2b562
PietariImmonen <pietari.immonen@gmail.com>
2022-05-24 12:52:45 +0300

    5.3

--- teht1/index.js
+++ teht1/index.js
@@ -39,6 +39,17 @@ app.get('/info', (req, res) => {
     const length = persons.length
     const time = new Date()
     res.json(`This list has ${length} persons and the time was ${time}`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if(person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
   })
 
 const PORT = 3001

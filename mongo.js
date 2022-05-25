2f3bcad7bae6fa9c53fd1bcfae0889071ff39619
PietariImmonen <pietari.immonen@gmail.com>
2022-05-25 09:45:21 +0300

    3.12

---
+++ mongo.js
@@ -0,0 +1,42 @@
+//const password1 = QBizhsYFTNZsWX3M
+
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+  `mongodb+srv://FullStack:${password}@cluster0.7iorj.mongodb.net/personApp?retryWrites=true&w=majority`
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', noteSchema)
+
+const person = new Person({
+  name: name,
+  number: number,
+})
+if(name && number) {
+    person.save().then(result => {
+        console.log(`Added ${name} number ${number} to the phonebook`)
+        mongoose.connection.close()
+      })
+} else {
+    Person.find({}).then(result => {
+        result.forEach(person => {
+          console.log(person)
+        })
+        mongoose.connection.close()
+      })
+}
+
